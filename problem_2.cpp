eb2bd60357cacf6848b5effb583562165df64471
Veler Jevsky <notspammeplz@outlook.com>
2025-10-29 20:36:17 +0300

    задача 2

--- problem_2.cpp
+++ problem_2.cpp
@@ -2,46 +2,67 @@
  *  Link: https://leetcode.com/problems/add-two-numbers/description/
 */
 
+#include <cstdlib>
+
 #include <gtest/gtest.h>
 
+namespace
+{
 struct ListNode
 {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+bool compareListNodes(const ListNode* lhs, const ListNode* rhs)
+{
+    if (nullptr == lhs && nullptr == rhs)
+        return true;
+
+    auto currLhs = lhs;
+    auto currRhs = rhs;
+
+    auto isEqual {true};
+
+    while (currLhs != nullptr && currRhs != nullptr)
+    {
+        if ((currLhs != nullptr && nullptr == currRhs) || (nullptr == currLhs && currRhs != nullptr))
+        {
+            isEqual = false;
+            break;
+        }
+
+        if (currLhs->val != currRhs->val)
+        {
+            isEqual = false;
+            break;
+        }
+
+        currLhs = currLhs->next;
+        currRhs = currRhs->next;
+    }
+
+    return isEqual;
+}
 
 ListNode* createListNode(std::initializer_list<int> list)
 {
-  if (std::empty(list))
-    return new ListNode();
+    if (std::empty(list))
+        return new ListNode();
 
-  auto listNode = new ListNode(*list.begin());
-  auto res = listNode;
+    auto listNode = new ListNode(*list.begin());
+    auto res = listNode;
 
-  for (auto iter = list.begin() + 1; iter != list.end(); ++iter)
-  {
-    res->next = new ListNode(*iter);
-    res = res->next;
-  }
-
-  return listNode;
-}
-
-void printList(ListNode const* list)
-{
-    if (nullptr == list)
-        return;
-
-    auto curr = list;
-    while (curr != nullptr)
+    for (auto iter = list.begin() + 1; iter != list.end(); ++iter)
     {
-        std::cout << curr->val << " ";
-        curr = curr->next;
+        res->next = new ListNode(*iter);
+        res = res->next;
     }
-    std::cout << "\n";
+
+    return listNode;
 }
 
 void printListRecursive(ListNode const* list)
@@ -55,40 +76,84 @@ void printListRecursive(ListNode const* list)
     std::cout << list->val << " ";
     printListRecursive(list->next);
 }
-
-void addFirstToSecond(ListNode const* list1, ListNode* list2)
-{
-    if (nullptr == list1)
-    {
-        return;
-    }
-
-    // list2->val
-    // printListRecursive(list->next);
 }
 
-// class Solution
-// {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-//     {
-//         if (nullptr == l1 && nullptr == l2)
-//             return nullptr;
+class Solution
+{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        if (nullptr == l1 && nullptr == l2)
+            return nullptr;
 
-//         if (l1->next)
+        auto currL1 = l1;
+        auto currL2 = l2;
+        auto res = new ListNode();
+        auto currRes = res;
+        auto rem {0};
+        div_t dv {};
 
-//         auto res = new ListNode();
+        while (currL1 != nullptr || currL2 != nullptr)
+        {
+            if (currL1)
+            {
+                currRes->val += currL1->val;
+                currL1 = currL1->next;
+            }
 
-//         return res;
-//     }
-// };
+            if (currL2)
+            {
+                currRes->val += currL2->val;
+                currL2 = currL2->next;
+            }
+
+            currRes->val += rem;
+
+            if (currRes->val > 9)
+            {
+                dv = std::div(currRes->val, 10);
+                rem = dv.quot;
+                currRes->val = dv.rem;
+            }
+            else
+            {
+                rem = 0;
+            }
+
+            if (nullptr == currL1 && nullptr == currL2)
+                break;
+
+            currRes->next = new ListNode();
+            currRes = currRes->next;
+        }
+
+        if (rem > 0)
+        {
+            if (rem > 9)
+            {
+                dv = std::div(rem, 10);
+                currRes->val = dv.quot;
+                currRes->next = new ListNode(dv.rem);
+            }
+            else
+            {
+                currRes->next = new ListNode(rem);
+            }
+        }
+        return res;
+    }
+};
 
 struct TestWithParameters : public testing::TestWithParam<std::tuple<ListNode*, ListNode*, ListNode*>> {};
 TEST_P(TestWithParameters, Problem_2)
 {
     auto [list1, list2, expectList] = GetParam();
-    printListRecursive(expectList);
-    SUCCEED();
+
+    Solution solver {};
+    const auto res = solver.addTwoNumbers(list1,  list2);
+    printListRecursive(res);
+
+    EXPECT_TRUE(compareListNodes(res, expectList));
 }
 INSTANTIATE_TEST_SUITE_P(TestGroupName, TestWithParameters, testing::Values(
     std::make_tuple(createListNode({1, 2, 3}), createListNode({4, 5, 6}), createListNode({5, 7, 9})),
